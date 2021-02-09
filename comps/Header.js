5375893ab6207acc83df5e36429eb176b5a0826e
Rakesh DR <rakeshdr543@gmail.com>
2021-02-10 01:29:29 +0530

    Added linking between pages

--- comps/Header.js
+++ comps/Header.js
@@ -1,10 +1,11 @@
+import Link from 'next/link'
 const Header = () => {
     return ( 
         <div>
             <h2>Ninja list</h2>
-            <a>Home</a>
-            <a>About</a>
-            <a>Ninja Listing</a>
+            <Link href='/'><a>Home</a></Link>
+            <Link href='/about'><a>About</a></Link>
+            <Link href='/ninjas'><a>Ninja Listing</a></Link>
         </div>
      );
 }
