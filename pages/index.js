b6f489713d0afd88089cd6797a368eec2226b65b
Rakesh DR <rakeshdr543@gmail.com>
2021-02-10 01:39:40 +0530

    Added layout component

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,16 @@ import Link from 'next/link'
 import Header from '../comps/Header'
 import Footer from '../comps/Footer'
 import styles from '../styles/Home.module.css'
+import Layout from '../comps/Layout'
 
 export default function Home() {
   return (
     <div>
-      <Header />
      <h2>Home Page</h2>
      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
      
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     <Link href='/ninjas'><a>See more Ninja Listing</a></Link>
-    <Footer />
     </div>
   )
 }
