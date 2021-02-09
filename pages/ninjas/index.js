1778ef9baa02220678ad3a77d23d8e50b6b7f7b8
Rakesh DR <rakeshdr543@gmail.com>
2021-02-10 01:07:26 +0530

    Added next routes

---
+++ pages/ninjas/index.js
@@ -0,0 +1,7 @@
+const Ninjas = () => {
+    return (  <h2>
+        Ninjas page
+    </h2>);
+}
+ 
+export default Ninjas;
---
+++ pages/ninjas/test.js
@@ -0,0 +1,7 @@
+const Test = () => {
+    return ( <h2>
+        Test Ninjas page
+    </h2> );
+}
+ 
+export default Test;
