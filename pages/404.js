dfe2fb07c8bc0547433ca39ed7468ca22aa040a3
Rakesh DR <rakeshdr543@gmail.com>
2021-02-19 01:57:19 +0530

    Added static props

--- pages/404.js
+++ pages/404.js
@@ -11,7 +11,7 @@ const NotFound = () => {
     }, [])
     return ( 
         <div className='not-found'>
-            <h1>Oooops ...</h1>
+            <h1>Oooops  ...</h1>
             <h2>That page cannot be found.</h2>
             <p>Go back to the <Link href='/'><a>Homepage</a></Link></p>
         </div>
