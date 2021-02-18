306a2650b56a87585d9c36aa22b673a8ad718c21
Rakesh DR <rakeshdr543@gmail.com>
2021-02-19 01:55:30 +0530

    Added static props

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -3,7 +3,6 @@ import styles from '../../styles/Ninjas.module.css'
 export const getStaticProps = async() =>{
     const res = await fetch("https://jsonplaceholder.typicode.com/users")
     const data = await res.json()
-
     return {
         props:{ninjas:data}
     }
