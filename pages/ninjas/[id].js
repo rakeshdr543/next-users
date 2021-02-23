3af8876c209ce4f82fa447f2e361e565f3f6206b
Rakesh DR <rakeshdr543@gmail.com>
2021-02-23 06:02:27 +0530

    Added template string

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,5 +1,5 @@
 export const getStaticPaths = async() =>{
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
     const data = await res.json()
 
     const paths = data.map(ninja=>{
