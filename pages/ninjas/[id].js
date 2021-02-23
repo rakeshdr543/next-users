2b55fd25ffae75d98ce273ab286805e86c42635f
Rakesh DR <rakeshdr543@gmail.com>
2021-02-23 06:00:28 +0530

    Added dynamic page

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -14,11 +14,24 @@ export const getStaticPaths = async() =>{
     }
 }
 
-const Details = () => {
+export const getStaticProps = async(context) =>{
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/'+id)
+    const data = await res.json()
+    
+    return {
+        props:{ninja:data}
+    }
+}
+
+const Details = ({ninja}) => {
     return (
-        <>
-          <h1>Detail Page</h1> 
-        </> 
+        <div>
+          <h1>{ninja.name}</h1>
+          <p>{ninja.email}</p>
+          <p>{ninja.website}</p> 
+          <p>{ninja.address.city}</p>
+        </div> 
      );
 }
  
