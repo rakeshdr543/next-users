221730659ce37a6fc1c38f869f2b33f89e5b1b56
Rakesh DR <rakeshdr543@gmail.com>
2021-02-23 06:01:32 +0530

    Added template string

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -16,7 +16,7 @@ export const getStaticPaths = async() =>{
 
 export const getStaticProps = async(context) =>{
     const id = context.params.id;
-    const res = await fetch('https://jsonplaceholder.typicode.com/users/'+id)
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
     const data = await res.json()
     
     return {
